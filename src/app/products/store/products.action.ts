6308975dd231e5f25e1191727eaeaec91ea0e7ba
Dmitry Vlasyuk <vlasyukdmitry2002@gmail.com>
2023-03-27 16:15:10 +0300

    1st

--- src/app/products/store/products.action.ts
+++ src/app/products/store/products.action.ts
@@ -1,5 +1,6 @@
 import { createAsyncThunk } from "@reduxjs/toolkit";
 import repository from "../../../repository";
+import { ProductsDto } from "../types/product-dto.type";
 
 const headers = {
   headers: {
@@ -19,3 +20,51 @@ export const getProducts = createAsyncThunk(
     }
   }
 );
+
+export const getProductById = createAsyncThunk<
+  ProductsDto,
+  { productId: number }
+>("products/getById", async ({ productId }, thunkAPI) => {
+  try {
+    const response = await repository.get(`/products/${productId}`, headers);
+    return response.data;
+  } catch (e) {
+    return thunkAPI.rejectWithValue("Can`t get product.");
+  }
+});
+
+export const createProduct = createAsyncThunk<
+  ProductsDto,
+  { dto: ProductsDto }
+>("products/create", async ({ dto }, thinkAPI) => {
+  try {
+    const response = await repository.post("/products", dto, headers);
+    return response.data;
+  } catch (e) {
+    return thinkAPI.rejectWithValue("Can`t create product.");
+  }
+});
+
+export const updateProduct = createAsyncThunk<
+  ProductsDto,
+  { productId: number; dto: ProductsDto }
+>("products/update", async ({ productId, dto }, thinkAPI) => {
+  try {
+    const response = await repository.put(`/products/${productId}`, dto, headers);
+    return response.data;
+  } catch (e) {
+    return thinkAPI.rejectWithValue("Can`t update product.");
+  }
+});
+
+export const deleteProduct = createAsyncThunk<
+  ProductsDto,
+  { productId: number }
+>("products/delete", async ({ productId }, thunkAPI) => {
+  try {
+    const response = await repository.delete(`/products/${productId}`, headers);
+    return response.data;
+  } catch (e) {
+    return thunkAPI.rejectWithValue("Can`t delete product.");
+  }
+});
