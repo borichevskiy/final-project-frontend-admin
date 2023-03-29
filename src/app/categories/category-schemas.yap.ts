023b9f78fd6e519ec98952e38838d37b6020b909
ReginaShushko <petrova.regina.mihajlovna@gamil.com>
2023-03-29 18:15:49 +0300

    Add category form validation

---
+++ src/app/categories/category-schemas.yap.ts
@@ -0,0 +1,15 @@
+import * as yup from "yup";
+
+export const schemaCreateCategory = yup
+  .object()
+  .shape({
+    name: yup
+      .string()
+      .max(40, "Name must be less than 100 characters")
+      .required("Name is a required field"),
+    description: yup
+      .string()
+      .max(100, "Max 100 symbols")
+      .required("Description is a required field"),
+  })
+  .required();
