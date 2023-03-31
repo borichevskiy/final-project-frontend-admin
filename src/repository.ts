b7d4d01a242dd4999e35bcfab3a0a135b2636b23
borichevskiy <81485449+borichevskiy@users.noreply.github.com>
2023-03-31 17:56:02 +0300

    Update repository.ts
    swap port in base url

--- src/repository.ts
+++ src/repository.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 import Cookies from "js-cookie";
 
-const REACT_APP_API_URL  = 'http://localhost:5000';
+const REACT_APP_API_URL  = 'http://localhost:3000';
 
 const repository = axios.create({
     baseURL: REACT_APP_API_URL
